fb35c842d27fbbbe6f955d2304aaccdaa200268b
Danimal <runiondd@gmail.com>
2018-12-06 21:13:35 -0500

    add images folder

--- liri.js
+++ liri.js
@@ -35,15 +35,18 @@ switch (command){
         var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
         axios.get(queryUrl).then(
             function(response) {
-                if(response.data[0] ===  undefined) {
+                if(response.data[0].venue !=  undefined) {
                     console.log("Event Veunue: " + response.data[0].venue.name);
                     console.log("Event Location: " + response.data[0].venue.city);
                     var eventDateTime = moment(response.data[0].datetime);
                     console.log("Event Date & Time: " + eventDateTime.format("dddd, MMMM Do YYYY"));
                 }
+                else {
+                    console.log("No results found.");
+                }
             }
-        ).catch(function (error) {  
-            console.log("No Results.")
+        ).catch(function (error) {
+            console.log (error);
       });
     }
 
